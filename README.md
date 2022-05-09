7728ed8b9d5795b9eef02de88ecbf6c142bff203
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-05-09 19:04:51 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+## 시작 2021.3.30. ~ ing
+- 참고1: Vuejs로 게시판 만들기, 개린이르라나
+- 참고링크1: https://youtu.be/s1lXVr65KZg
+- 참고2: node.js로 서버 만들기, 개린이르라나
+- 링크2: https://youtu.be/4SUGu3YBZ_A
+> 실행 방법: 해당 폴더로 이동, npm run dev 또는 yarn start
---
+++ bootstrap-bbs/src/data/index.js
@@ -0,0 +1,297 @@
+export default {
+    User: [
+        {
+            user_id: 1,
+            name: 'lelana',
+            created_at: '2018-09-11 11:42:11'
+        },
+        {
+            user_id: 2,
+            name: '슬기',
+            created_at: '2018-09-11 11:42:11'
+        },
+        {
+            user_id: 3,
+            name: '조이',
+            created_at: '2018-09-11 11:42:11'
+        }
+    ],
+    Content: [
+        {
+            content_id: 1,
+            user_id: 1,
+            title: '개린이 르라나의 강의 알림표',
+            context: '아직 미정입니다.',
+            created_at: '2019-01-01 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 2,
+            user_id: 3,
+            title: 'Vuejs로 게시판 만들기',
+            context: '연습중!',
+            created_at: '2019-01-02 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 3,
+            user_id: 2,
+            title: '제 생일은 아직 멀었어요!',
+            context: '^-^*',
+            created_at: '2019-03-29 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 4,
+            user_id: 1,
+            title: '개린이 르라나의 강의 알림표',
+            context: '아직 미정입니다.',
+            created_at: '2019-01-01 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 5,
+            user_id: 3,
+            title: 'Vuejs로 게시판 만들기',
+            context: '연습중!',
+            created_at: '2019-01-02 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 6,
+            user_id: 2,
+            title: '제 생일은 아직 멀었어요!',
+            context: '^-^*',
+            created_at: '2019-03-29 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 7,
+            user_id: 1,
+            title: '개린이 르라나의 강의 알림표',
+            context: '아직 미정입니다.',
+            created_at: '2019-01-01 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 8,
+            user_id: 3,
+            title: 'Vuejs로 게시판 만들기',
+            context: '연습중!',
+            created_at: '2019-01-02 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 9,
+            user_id: 2,
+            title: '제 생일은 아직 멀었어요!',
+            context: '^-^*',
+            created_at: '2019-03-29 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 10,
+            user_id: 1,
+            title: '개린이 르라나의 강의 알림표',
+            context: '아직 미정입니다.',
+            created_at: '2019-01-01 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 11,
+            user_id: 3,
+            title: 'Vuejs로 게시판 만들기',
+            context: '연습중!',
+            created_at: '2019-01-02 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 12,
+            user_id: 2,
+            title: '제 생일은 아직 멀었어요!',
+            context: '^-^*',
+            created_at: '2019-03-29 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 13,
+            user_id: 1,
+            title: '개린이 르라나의 강의 알림표',
+            context: '아직 미정입니다.',
+            created_at: '2019-01-01 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 14,
+            user_id: 3,
+            title: 'Vuejs로 게시판 만들기',
+            context: '연습중!',
+            created_at: '2019-01-02 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 15,
+            user_id: 2,
+            title: '제 생일은 아직 멀었어요!',
+            context: '^-^*',
+            created_at: '2019-03-29 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 16,
+            user_id: 1,
+            title: '개린이 르라나의 강의 알림표',
+            context: '아직 미정입니다.',
+            created_at: '2019-01-01 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 17,
+            user_id: 3,
+            title: 'Vuejs로 게시판 만들기',
+            context: '연습중!',
+            created_at: '2019-01-02 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 18,
+            user_id: 2,
+            title: '제 생일은 아직 멀었어요!',
+            context: '^-^*',
+            created_at: '2019-03-29 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 19,
+            user_id: 1,
+            title: '개린이 르라나의 강의 알림표',
+            context: '아직 미정입니다.',
+            created_at: '2019-01-01 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 20,
+            user_id: 3,
+            title: 'Vuejs로 게시판 만들기',
+            context: '연습중!',
+            created_at: '2019-01-02 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 21,
+            user_id: 2,
+            title: '제 생일은 아직 멀었어요!',
+            context: '^-^*',
+            created_at: '2019-03-29 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 22,
+            user_id: 1,
+            title: '개린이 르라나의 강의 알림표',
+            context: '아직 미정입니다.',
+            created_at: '2019-01-01 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 23,
+            user_id: 3,
+            title: 'Vuejs로 게시판 만들기',
+            context: '연습중!',
+            created_at: '2019-01-02 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 24,
+            user_id: 2,
+            title: '제 생일은 아직 멀었어요!',
+            context: '^-^*',
+            created_at: '2019-03-29 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 25,
+            user_id: 1,
+            title: '개린이 르라나의 강의 알림표',
+            context: '아직 미정입니다.',
+            created_at: '2019-01-01 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 26,
+            user_id: 3,
+            title: 'Vuejs로 게시판 만들기',
+            context: '연습중!',
+            created_at: '2019-01-02 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 27,
+            user_id: 2,
+            title: '제 생일은 아직 멀었어요!',
+            context: '^-^*',
+            created_at: '2019-03-29 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 28,
+            user_id: 1,
+            title: '개린이 르라나의 강의 알림표',
+            context: '아직 미정입니다.',
+            created_at: '2019-01-01 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 29,
+            user_id: 3,
+            title: 'Vuejs로 게시판 만들기',
+            context: '연습중!',
+            created_at: '2019-01-02 13:11:42',
+            updated_at: null
+        },
+        {
+            content_id: 30,
+            user_id: 2,
+            title: '제 생일은 아직 멀었어요!',
+            context: '^-^*',
+            created_at: '2019-03-29 13:11:42',
+            updated_at: null
+        },
+    ],
+    Comment: [
+        {
+            comment_id: 1,
+            user_id: 3,
+            content_id: 3,
+            context: '생일 축하해요!',
+            created_at: '2019-03-29 14:11:11',
+            updated_at: null
+        },
+        {
+            comment_id: 2,
+            user_id: 3,
+            content_id: 3,
+            context: '수민언니 생일 축하해요!',
+            created_at: '2019-03-29 16:11:11',
+            updated_at: null
+        },
+        {
+            comment_id: 3,
+            user_id: 2,
+            content_id: 1,
+            context: '강의 잘 보고 있습니다 ^^',
+            created_at: '2019-03-29 14:11:11',
+            updated_at: null
+        }
+    ],
+    SubComment: [
+        {
+            subcomment_id: 1,
+            comment_id: 3,
+            user_id: 1,
+            context: '오 영광입니다 ㅠㅠ!!',
+            created_at: '2019-03-29 16:22:11',
+            updated_at: null
+        }
+    ]
+}
