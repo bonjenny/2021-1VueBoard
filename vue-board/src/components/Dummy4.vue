06fd63c9026582338fa2e51c2e944c0d52b3ea39
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-07-29 14:21:30 +0900

    Add files via upload

---
+++ vue-board/src/components/Dummy4.vue
@@ -0,0 +1,542 @@
+<template>
+    <div>
+        <h1 id="home"><a href="../index.html">HOME</a></h1>
+        <ul id="menu">
+        <li><a href="../hw01/intro.html">HW01</a></li>
+        <li><a href="../hw02/hello-css03.html">HW02</a></li>
+        <li><a href="../hw03/hw03.html">HW03</a></li>
+        <li><a href="../hw04/hw04.html">HW04</a></li>
+        <li><a href="../hw07/hw07.html">HW07</a></li>
+        <li><a href="../hw08/hw08.html">HW08</a></li>
+        <li><a href="../hw09/portfolio.html">HW09</a></li>
+        </ul><br><br>
+
+        <h1 id="title">마카롱 만드는 법!</h1>
+        <p id="start">
+        <a href="https://blog.naver.com/pul_saem/221875919314" target="_blank">
+            <img src="./img/1.jpg" alt="1-초코마카롱"></a><br><br>
+        <!-- 이미지를 누르면 원본 블로그로 이동  -->
+        홈 베이킹 중에서도 난이도 '상'을 자랑하는
+        초코 마카롱 만들기! <br>
+        마카롱 만드는 법을 지금부터 알아볼까요?
+        </p><br>
+
+
+    <!-- 재료 준비 -->
+        <h2>홈 베이킹 초코 마카롱 만들기</h2>
+        <h3>
+        마카롱 만드는 법 <br>
+        : 재료 준비
+        </h3><br>
+        <img src="./img/2.jpg" alt="2-재료준비">
+        <h4>_food ingredients</h4>
+        <div>&star; 꼬끄 재료</div>
+        <ul>
+        <li>달걀 흰자 63g</li>
+        <li>설탕 60g</li>
+        <li>코코아파우더 7g</li>
+        <li>아몬드가루 82g</li>
+        <li>분당(슈가파우더) 75g</li>
+        </ul>
+        <div>&star; 가나슈 필링 재료</div>
+        <ul>
+        <li>생크림 100g</li>
+        <li>물엿 15g</li>
+        <li>다크커버춰 100g</li>
+        <li>무염 버터 10g</li>
+        <li>토핑용 카카오닙스 약간</li>
+        </ul><br>
+
+    <!-- 사전 준비 -->
+        <h2>홈 베이킹 초코 마카롱 만들기</h2>
+        <h3>
+        마카롱 만드는 법 <br>
+        : 사전 준비
+        </h3><br>
+        <img src="./img/3.jpg" alt="3-사전준비">
+        <p>
+        재료 외에도 마카롱을 만들기 전에 <br>
+        미리 준비해야 할 부분들이 있어요!
+        </p>
+        <p>
+        &#128151; 짤주머니에 1cm 원형 깍지를 넣어 <br>
+        꼬끄 반죽을 넣을 수 있게 준비해주세요.
+        </p>
+        <p>
+        &#128151; 팬닝힐 테프론시트 아래 3.5cm 크기의 <br>
+        마카롱 도안도 미리 준비하면 <br>
+        균일한 크기의 마카롱을 만들 수 있어요!
+        </p>
+        <p>
+        &#128151; 계란 흰자는 3일 동안 냉장 저장해주세요.
+        </p>
+        <p>
+        &#128151; 버터와 초콜렛, 생크림은 찬 기운이 빠지도록 <br>
+        실온에 둔 것을 사용해주세요.
+        </p><br>
+        <!-- &#128151; 💗 -->
+
+    <!-- 필링 만들기 -->
+        <h2>홈 베이킹 초코 마카롱 만들기</h2>
+        <h3>
+        마카롱 만드는 법 <br>
+        : 필링 만들기
+        </h3><br>
+        <img src="./img/4.jpg" alt="4-생크림데우기">
+        <p>
+        먼저, 마카롱 필링을 만들어 볼게요 &#128518; <br>
+        실온 상태의 생크림에 물엿을 넣고 <br>
+        전자레인지에 약 1분 30초 정도 돌려 <br>
+        75~80도 정도의 온도가 되도록 데워주세요.
+        </p>
+        <img src="./img/5.jpg" alt="5-생크림넣기">
+        <p>
+        그런 다음, 초콜릿이 담긴 볼에 <br>
+        전자레인지에 데운 생크림을 넣어주세요! <br>
+        1분 정도 그대로 둬서 <br>
+        생크림과 초콜릿의 온도를 맞춰줍니다.
+        </p>
+        <img class="img" src="./img/6.jpg" alt="6-초콜릿섞기">
+        <img class="img" src="./img/7.jpg" alt="7-초콜릿섞기2">
+        <p>
+        쉐킷 쉐킷 ٩(*•̀ᴗ•́*)و <br>
+        초콜릿은 녹아서 윤기가 날 때까지 잘 저어주세요
+        </p>
+        <img src="./img/8.jpg" alt="8-버터넣기">
+        <p>
+        말랑해진 실온의 버터도 넣어 <br>
+        덩어리가 없어질 때까지 저어주세요!
+        </p>
+        <p>
+        만들어진 필링은 짤주머니에 담아 <br>
+        냉장고에 넣어 짜기 쉽게 살짝 굳혀주세요.
+        </p><br>
+
+    <!-- 꼬끄 만들기 -->
+        <h2>홈 베이킹 초코 마카롱 만들기</h2>
+        <h3>
+        마카롱 만드는 법 <br>
+        : 꼬끄 만들기
+        </h3><br>
+        <img src="./img/9.jpg" alt="9-재료갈기">
+        <p>
+        이제 마카롱의 꼬끄를 만들어 볼게요! &#128522;
+        <!-- &#128522; 😆 -->
+        </p>
+        <p>
+        먼저 아몬드 가루와 분당, 코코아 가루를 <br>
+        푸드 프로세서에 넣어 7초간 돌려주새오. <br>
+        이렇게 하면 입자를 곱게 만들어 줄 수 있는데요. <br>
+        오래 돌릴 경우 아몬드 가루에서 유분이 <br>
+        빠져나올 수 있기 때문에 길게 돌리지 않는 게 좋아요.
+        </p>
+        <img src="./img/10.jpg" alt="10-체치기">
+        <p>
+        가루는 두 번 정도 체를 쳐서 준비합니다!
+        </p>
+        <img src="./img/11.jpg" alt="11-거품만들기">
+        <p>
+        3일 동안 냉장 저장한 흰자는 <br>
+        볼에 담아 중속으로 거품기를 돌려 <br>
+        거품을 만들어주세요.
+        </p>
+        <img src="./img/12.jpg" alt="12-머랭올리기">
+        <p>
+        거품의 입자가 미세해지면 <br>
+        설탕을 1/3 넣고 중속으로 돌려주세요. <br>
+        30초 간격으로 총 세 번 설탕을 나눠 넣으면서 <br>
+        중속으로 머랭을 올려줍니다!
+        </p>
+        <img src="./img/13.jpg" alt="13-머랭완성">
+        <p>
+        머랭에서 윤기가 나고 <br>
+        새부리모양으로 휘는 뿔이 만들어지면 <br>
+        2분간 1단으로 거품을 안정화한 후 <br>
+        머랭 치기를 완료합니다.
+        </p>
+        <img src="./img/14.jpg" alt="14-재료섞기">
+        <p>
+        체에 쳐 둔 가루의 반을 머랭에 넣어 가르듯 섞어주세요.
+        </p>
+        <img src="./img/15.jpg" alt="15-재료섞기2">
+        <p>
+        완전히 섞이면 나머지 반도 넣어 섞어주세요 <br>
+        바닥에 섞이지 않은 가루가 가라앉아있는 경우가 많으니 <br>
+        바닥까지 꼼꼼히 긁어 섞어줘야 해요 &#128064;
+        <!-- &#128064; 👀 -->
+        </p>
+        <img src="./img/16.jpg" alt="16-마카로나주시작">
+        <p>
+        이제 마카로나주를 시작할 거예요. <br>
+        마카로나주는 머랭 속의 공기를 제거해서 <br>
+        반죽 농도를 조절하는 과정을 뜻하는데요. <br>
+        반죽을 볼의 벽으로 밀어 바르고 <br>
+        다시 긁어모으기를 반복해서 <br>
+        반죽 속 거품을 꺼뜨려 안정화해줍니다!
+        </p>
+        <img src="./img/17.jpg" alt="17-마카로나주끝">
+        <p>
+        반죽을 떨어뜨렸을 때 끊어지지 않고 <br>
+        무겁게 계단 모양으로 접히듯 떨어지면 완성이에요. <br>
+        마카로나주 작업이 부족하거나 과한 경우, <br>
+        코크가 잘 구워지지 않으니 <br>
+        꼼꼼하게 살펴보고 연습해주세요!
+        </p>
+        <img src="./img/18.jpg" alt="18-팬닝">
+        <p>
+        완성된 반죽은 짤주머니에 담아 <span>팬닝</span>해주세요. <br>
+        오븐팬을 들고 다른 한 손으로 바닥을 쳐서 <br>
+        기포를 빼준 후 말려줍니다.
+        </p>
+        <p style="font-size: 0.75em;"><span>*팬닝</span> : 반죽을 빵틀에 채우거나 철판에 나열하는 일</p>
+        <img src="./img/19.jpg" alt="19-말리기">
+        <p>
+        반죽을 손으로 만졌을 때 손에 묻어나는 것이 없되 <br>
+        속은 말랑한 정도가 적당해요! <br>
+        표면이 어느 정도 마르면 윗면에 <br>
+        카카오닙스를 살짝 뿌려 구워주면 좋아요.
+        </p>
+        <p>
+        150도로 예열한 오븐에 140도로 14분간 구워주세요. <br>
+        오븐에 따라 굽는 시간과 온도는 <br>
+        조금씩 차이 날 수 있답니다.
+        </p>
+        <img src="./img/20.jpg" alt="20-꼬끄굽기">
+        <p>
+        사진은 구운 직후 꼬끄 모습이에요! <br>
+        벌써 기대가 되는데요  ˘◡˘ <br>
+        구운 뒤 바로 꼬끄에 손을 대면 <br>
+        카카오닙스가 떨어질 수 있으니 한 김 식혀주세요.
+        </p>
+        <img src="./img/21.jpg" alt="21-짝맞추기">
+        <p>
+        카카오닙스가 뿌려진 것을 윗부분으로 정하고 <br>
+        크기를 맞춰 짝을 지어 줍니다.
+        </p>
+        <img class="img" src="./img/22.jpg" alt="22-필링얹기">
+        <img class="img" src="./img/23.jpg" alt="23-꼬끄덮기">
+        <p>
+        살짝 굳힌 가나슈 필링을 꼬끄 위에 올린 뒤 덮어서 <br>
+        냉장고나 냉동고에 굳혀 모양을 잡아주면 완성!
+        </p>
+        <img src="./img/24.jpg" alt="24-숙성시키기">
+        <p>
+        냉동고에서 한 시간 정도 굳힌 마카롱은 다시 포장해서 <br>
+        하루 정도 냉장고에 숙성 후 먹으면 정말 맛있어요.
+        </p>
+        <img src="./img/25.jpg" alt="25-가나슈필링">
+        <p>
+        가나슈 필링은 기본적인 마카롱 필링 중 하나인데요. <br>
+        가나슈 필링을 넣은 초코 마카롱은 <br>
+        세계적으로도 큰 사랑을 받고 있다고 해요 &#128525;
+        <!-- &#128525; 😍 -->
+        </p>
+        <img src="./img/26.jpg" alt="26-초코마카롱완성">
+        <p>
+        주변 친구들에게 선물하기도 좋은 <br>
+        초코 마카롱 만들기!
+        </p>
+        <img class="img" src="./img/27.png" alt="27-이모티콘">
+        <p>
+        풀샘이가 소개한 마카롱 만드는 법으로 <br>
+        홈 베이킹에 도전해보는 거 어때요? <br>
+        ( ๑ ᴖ ᴈ ᴖ)
+        </p>
+        <br><hr><br>
+        <img src="./img/28.jpg" alt="28-끝">
+        <p>
+        <span>
+            #마카롱 #초코마카롱 #마카롱만들기 #마카롱만드는법 #초코마카롱만들기
+            #초코마카롱만드는 #홈베이킹 #베이킹
+        </span><br><br><br>
+        출처 : 마카롱 만드는 법!
+        홈 베이킹 초코 마카롱 만들기|작성자 풀샘 <br>
+        <a href="https://blog.naver.com/pul_saem/221875919314" target="_blank">
+            >>여기를 클릭!</a>
+        </p><br>
+        <input v-model="writer" type="text" placeholder="글쓴이"/>
+        <input v-model="title" type="text" placeholder="제목"/>
+        <textarea v-model="content" placeholder="내용"/>
+        <input v-model="writer" type="text" placeholder="글쓴이"/>
+        <input v-model="title" type="text" placeholder="제목"/>
+        <textarea v-model="content" placeholder="내용"/>
+        <input v-model="writer" type="text" placeholder="글쓴이"/>
+        <input v-model="title" type="text" placeholder="제목"/>
+        <textarea v-model="content" placeholder="내용"/>
+        <h1 id="home"><a href="../index.html">HOME</a></h1>
+        <ul id="menu">
+        <li><a href="../hw01/intro.html">HW01</a></li>
+        <li><a href="../hw02/hello-css03.html">HW02</a></li>
+        <li><a href="../hw03/hw03.html">HW03</a></li>
+        <li><a href="../hw04/hw04.html">HW04</a></li>
+        <li><a href="../hw07/hw07.html">HW07</a></li>
+        <li><a href="../hw08/hw08.html">HW08</a></li>
+        <li><a href="../hw09/portfolio.html">HW09</a></li>
+        </ul><br><br>
+
+        <h1 id="title">시크스포츠</h1>
+        <a href="https://smartstore.naver.com/seeksports/products/3296139538#revw"
+        target="_blank">
+        <h3 id="subtitle">
+        [삼천리] 도시형 로드/하이브리드 자전거 3종 특가 모음
+        </h3></a>
+        <hr id="box"><br>
+
+        <!-- [1] 씨티크루저 MV2007 -->
+        <h2>선택01</h2>
+        <h3>시티크루저 MV2007</h3>
+        <p>
+        다이아몬드형 하이텐강 미니벨로 자전거(470) <br>
+        20형 / 그립시프트 7단 변속시스템<br>
+        통학용, 출퇴근용, 레저용 권장 / 벨, 페달 포함
+        </p><hr>
+        <a href="https://smartstore.naver.com/seeksports/products/3296139538#revw"
+        target="_blank">
+        <img src="./img/1MV2007BY.PNG" alt="[1] 씨티크루저 MV2007 블랙/옐로우">
+        <img src="./img/1MV2007WR.PNG" alt="[1] 씨티크루저 MV2007 화이트/레드">
+        <img src="./img/1MV2007BR.PNG" alt="[1] 씨티크루저 MV2007 블랙/레드">
+        </a>
+
+        <h2>SPECIFICATION</h2>
+        <h3 class=specification>상세정보</h3><hr>
+        <h4>스펙</h4>
+        <ul>
+        <li>다이아몬드형 하이텐강 프레임</li>
+        <li>신장 약 140cm 후반~170cm 후반 승차권장</li>
+        <li>시마노(Shimano) 후리휠 / 그립시프트 7단 변속시스템</li>
+        <li>제동력 우수한 V-브레이크 / 알루미늄 브레이크 레버</li>
+        <li>켄다 20*1.3/8 (프레스타) 컬러 타이어 / 알루미늄 컬러 CNC 림</li>
+        <li>체인이탈방지 더블루프 체인가드 장착</li>
+        <li>사틴피니시 핸들바, 시트포스트 / CP도금 허브 / 컬러 그립</li>
+        <li>높낮이, 전후 위치조절 가능한 삼천리 컬러안장 / 물통케이지 장착가능</li>
+        <li>높낮이 조절 알루미늄 핸들스템</li>
+        <li>벨, 페달 포함</li>
+        </ul>
+        <h4>중량</h4>
+        <ul>
+        <li>약 12.6kg</li>
+        </ul>
+        <h4>색상</h4>
+        <ul>
+        <li>화이트/레드, 블랙/레드, 블랙/옐로우</li>
+        </ul>
+        <h4>기타</h4>
+        <ul>
+        <li>옵션상 98%조립 미선택시 <b>공장출고 반조립박스로 발송</b></li>
+        </ul><hr>
+
+        <!-- [2] 엘르 MV20 -->
+        <h2>선택02</h2>
+        <h3>엘르 MV20</h3>
+        <p>
+        하이텐강 저지상고 자전거<br>
+        20형 / 시마노 버튼식 7단 변속시스템<br>
+        통학용, 출퇴근용, 레저용 권장 / 벨, 페달 포함
+        </p><hr>
+        <a href="https://smartstore.naver.com/seeksports/products/3296139538#revw"
+        target="_blank">
+        <img src="./img/2MV20BR.PNG" alt="[2] 엘르 MV20 블랙/레드">
+        <img src="./img/2MV20BR.PNG" alt="[2] 엘르 MV20 밀크">
+        </a>
+
+        <h2>SPECIFICATION</h2>
+        <h3 class=specification>상세정보</h3><hr>
+        <h4>스펙</h4>
+        <ul>
+        <li>하이텐강 저지상고 프레임</li>
+        <li>신장 약 140cm 후반~170cm 후반 승차권장</li>
+        <li>시마노(Shimano) 버튼식 7단 변속시스템</li>
+        <li>제동력 우수한 V-브레이크</li>
+        <li>켄다 20*1.3/8 (프레스타) 컬러 타이어 / 알루미늄 컬러 림</li>
+        <li>체인이탈방지 더블루프 체인가드 장착</li>
+        <li>사틴피니시 핸들바, 시트포스트 / CP도금 허브 / 컬러 그립</li>
+        <li>높낮이, 전후 위치조절 가능한 삼천리 컬러안장 / 물통케이지 장착가능</li>
+        <li>높낮이 조절 알루미늄 핸들스템</li>
+        <li>벨, 페달 포함</li>
+        </ul>
+        <h4>중량</h4>
+        <ul>
+        <li>약 12kg 이상</li>
+        </ul>
+        <h4>색상</h4>
+        <ul>
+        <li>블랙/레드, 밀크</li>
+        </ul>
+        <h4>기타</h4>
+        <ul>
+        <li>옵션상 98%조립 미선택시 <b>공장출고 반조립박스로 발송</b></li>
+        </ul><hr>
+
+        <!-- [3] 씨티크루저 H7007 -->
+        <h2>선택03</h2>
+        <h3>씨티크루저 H7007</h3>
+        <p>
+        슬릭 하이텐강 하이브리드 자전거 (440, 510)<br>
+        700C / 그립시프트 7단 변속시스템<br>
+        통학용, 출퇴근용, 레저용 권장 / 벨, 페달 포함
+        </p><hr>
+        <a href="https://smartstore.naver.com/seeksports/products/3296139538#revw"
+        target="_blank">
+        <img src="./img/3H7007BR.PNG" alt="[3] 씨티크루저 H7007 블랙/레드">
+        <img src="./img/3H7007WGP.PNG" alt="[3] 씨티크루저 H7007 화이트/그린/퍼플">
+        <img src="./img/3H7007MK.PNG" alt="[3] 씨티크루저 H7007 밀크">
+        </a>
+
+        <h2>SPECIFICATION</h2>
+        <h3 class=specification>상세정보</h3><hr>
+        <h4>스펙</h4>
+        <ul>
+        <li>슬릭 하이텐강 하이브리드 프레임 (440, 510)</li>
+        <li>440차체 : 신장 약 150cm 중반~170cm 후반</li>
+        <li>510차체 : 신장 약 160cm 초반~180cm 초반 승차권장</li>
+        <li>시마노(Shimano) 스프라켓 / 그립시프트 7단 변속시스템</li>
+        <li>제동력 우수한 V-브레이크 / 알루미늄 브레이크 레버</li>
+        <li>700*28 PONELY 컬러 타이어 (프레스타밸브) / 알루미늄 컬러 이중 림</li>
+        <li>체인이탈방지 더블루프 체인가드 장착</li>
+        <li>알루미늄 높낮이조절 핸들스템, 하이브리드 컬러 홀 안장</li>
+        <li>높낮이, 전후 위치조절 가능한 안장 / 물통케이지 장착가능</li>
+        </ul>
+        <h4>중량</h4>
+        <ul>
+        <li>약 13kg 이상</li>
+        </ul>
+        <h4>색상</h4>
+        <ul>
+        <li>블랙/레드, 화이트/그린/퍼플, 밀크</li>
+        </ul>
+        <h4>기타</h4>
+        <ul>
+        <li>옵션상 98%조립 미선택시 <b>공장출고 반조립박스로 발송</b></li>
+        </ul><hr>
+    </div>
+</template>
+<script>
+import data from '@/data'
+export default {
+    name: 'Dummy',
+    data() {
+        const index = this.$route.params.contentId;
+        return {
+            data: data,
+            writer: "",
+            title: "",
+            content: "",
+            index: index,
+            writer: index !== undefined ? data[index].writer : "",
+            title: index !== undefined ? data[index].title : "",
+            content: index !== undefined ? data[index].content : ""
+        }
+    },
+    methods: {
+        write() {
+            this.data.push({
+                writer: this.writer,
+                title: this.title,
+                content: this.content
+            }),
+            this.$router.push({
+                path: '/'
+            })
+        },
+        update() {
+            data[this.index].writer = this.writer,
+            data[this.index].title = this.title,
+            data[this.index].content = this.content,
+            this.$router.push({
+                path: '/'
+            })
+        },
+        write1() {
+            this.data.push({
+                writer: this.writer,
+                title: this.title,
+                content: this.content
+            }),
+            this.$router.push({
+                path: '/'
+            })
+        },
+        update1() {
+            data[this.index].writer = this.writer,
+            data[this.index].title = this.title,
+            data[this.index].content = this.content,
+            this.$router.push({
+                path: '/'
+            })
+        },
+        write2() {
+            this.data.push({
+                writer: this.writer,
+                title: this.title,
+                content: this.content
+            }),
+            this.$router.push({
+                path: '/'
+            })
+        },
+        update2() {
+            data[this.index].writer = this.writer,
+            data[this.index].title = this.title,
+            data[this.index].content = this.content,
+            this.$router.push({
+                path: '/'
+            })
+        },
+        
+        write3() {
+            this.data.push({
+                writer: this.writer,
+                title: this.title,
+                content: this.content
+            }),
+            this.$router.push({
+                path: '/'
+            })
+        },
+        update3() {
+            data[this.index].writer = this.writer,
+            data[this.index].title = this.title,
+            data[this.index].content = this.content,
+            this.$router.push({
+                path: '/'
+            })
+        },
+        write4() {
+            this.data.push({
+                writer: this.writer,
+                title: this.title,
+                content: this.content
+            }),
+            this.$router.push({
+                path: '/'
+            })
+        },
+        update4() {
+            data[this.index].writer = this.writer,
+            data[this.index].title = this.title,
+            data[this.index].content = this.content,
+            this.$router.push({
+                path: '/'
+            })
+        },
+        write5() {
+            this.data.push({
+                writer: this.writer,
+                title: this.title,
+                content: this.content
+            }),
+            this.$router.push({
+                path: '/'
+            })
+        },
+        update5() {
+            data[this.index].writer = this.writer,
+            data[this.index].title = this.title,
+            data[this.index].content = this.content,
+            this.$router.push({
+                path: '/'
+            })
+        }
+    }
+}
+</script>
