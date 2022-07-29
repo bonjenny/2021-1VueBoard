3decc150c74a43e570243bd5a3784dd26dd6edac
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2022-07-29 14:18:58 +0900

    Add files via upload

---
+++ vue-board/src/components/Dummy2.vue
@@ -0,0 +1,288 @@
+<template>
+    <div>
+        <input v-model="writer" type="text" placeholder="글쓴이"/>
+        <input v-model="title" type="text" placeholder="제목"/>
+        <textarea v-model="content" placeholder="내용"/>
+        <input v-model="writer" type="text" placeholder="글쓴이"/>
+        <input v-model="title" type="text" placeholder="제목"/>
+        <textarea v-model="content" placeholder="내용"/>
+        <input v-model="writer" type="text" placeholder="글쓴이"/>
+        <input v-model="title" type="text" placeholder="제목"/>
+        <textarea v-model="content" placeholder="내용"/>
+        <h1 id="home"><a href="../index.html">HOME</a></h1>
+        <ul id="menu">
+        <li><a href="../hw01/intro.html">HW01</a></li>
+        <li><a href="../hw02/hello-css03.html">HW02</a></li>
+        <li><a href="../hw03/hw03.html">HW03</a></li>
+        <li><a href="../hw04/hw04.html">HW04</a></li>
+        <li><a href="../hw07/hw07.html">HW07</a></li>
+        <li><a href="../hw08/hw08.html">HW08</a></li>
+        <li><a href="../hw09/portfolio.html">HW09</a></li>
+        </ul><br><br>
+
+        <h1 id="title">시크스포츠</h1>
+        <a href="https://smartstore.naver.com/seeksports/products/3296139538#revw"
+        target="_blank">
+        <h3 id="subtitle">
+        [삼천리] 도시형 로드/하이브리드 자전거 3종 특가 모음
+        </h3></a>
+        <hr id="box"><br>
+
+        <!-- [1] 씨티크루저 MV2007 -->
+        <h2>선택01</h2>
+        <h3>시티크루저 MV2007</h3>
+        <p>
+        다이아몬드형 하이텐강 미니벨로 자전거(470) <br>
+        20형 / 그립시프트 7단 변속시스템<br>
+        통학용, 출퇴근용, 레저용 권장 / 벨, 페달 포함
+        </p><hr>
+        <a href="https://smartstore.naver.com/seeksports/products/3296139538#revw"
+        target="_blank">
+        <img src="./img/1MV2007BY.PNG" alt="[1] 씨티크루저 MV2007 블랙/옐로우">
+        <img src="./img/1MV2007WR.PNG" alt="[1] 씨티크루저 MV2007 화이트/레드">
+        <img src="./img/1MV2007BR.PNG" alt="[1] 씨티크루저 MV2007 블랙/레드">
+        </a>
+
+        <h2>SPECIFICATION</h2>
+        <h3 class=specification>상세정보</h3><hr>
+        <h4>스펙</h4>
+        <ul>
+        <li>다이아몬드형 하이텐강 프레임</li>
+        <li>신장 약 140cm 후반~170cm 후반 승차권장</li>
+        <li>시마노(Shimano) 후리휠 / 그립시프트 7단 변속시스템</li>
+        <li>제동력 우수한 V-브레이크 / 알루미늄 브레이크 레버</li>
+        <li>켄다 20*1.3/8 (프레스타) 컬러 타이어 / 알루미늄 컬러 CNC 림</li>
+        <li>체인이탈방지 더블루프 체인가드 장착</li>
+        <li>사틴피니시 핸들바, 시트포스트 / CP도금 허브 / 컬러 그립</li>
+        <li>높낮이, 전후 위치조절 가능한 삼천리 컬러안장 / 물통케이지 장착가능</li>
+        <li>높낮이 조절 알루미늄 핸들스템</li>
+        <li>벨, 페달 포함</li>
+        </ul>
+        <h4>중량</h4>
+        <ul>
+        <li>약 12.6kg</li>
+        </ul>
+        <h4>색상</h4>
+        <ul>
+        <li>화이트/레드, 블랙/레드, 블랙/옐로우</li>
+        </ul>
+        <h4>기타</h4>
+        <ul>
+        <li>옵션상 98%조립 미선택시 <b>공장출고 반조립박스로 발송</b></li>
+        </ul><hr>
+
+        <!-- [2] 엘르 MV20 -->
+        <h2>선택02</h2>
+        <h3>엘르 MV20</h3>
+        <p>
+        하이텐강 저지상고 자전거<br>
+        20형 / 시마노 버튼식 7단 변속시스템<br>
+        통학용, 출퇴근용, 레저용 권장 / 벨, 페달 포함
+        </p><hr>
+        <a href="https://smartstore.naver.com/seeksports/products/3296139538#revw"
+        target="_blank">
+        <img src="./img/2MV20BR.PNG" alt="[2] 엘르 MV20 블랙/레드">
+        <img src="./img/2MV20BR.PNG" alt="[2] 엘르 MV20 밀크">
+        </a>
+
+        <h2>SPECIFICATION</h2>
+        <h3 class=specification>상세정보</h3><hr>
+        <h4>스펙</h4>
+        <ul>
+        <li>하이텐강 저지상고 프레임</li>
+        <li>신장 약 140cm 후반~170cm 후반 승차권장</li>
+        <li>시마노(Shimano) 버튼식 7단 변속시스템</li>
+        <li>제동력 우수한 V-브레이크</li>
+        <li>켄다 20*1.3/8 (프레스타) 컬러 타이어 / 알루미늄 컬러 림</li>
+        <li>체인이탈방지 더블루프 체인가드 장착</li>
+        <li>사틴피니시 핸들바, 시트포스트 / CP도금 허브 / 컬러 그립</li>
+        <li>높낮이, 전후 위치조절 가능한 삼천리 컬러안장 / 물통케이지 장착가능</li>
+        <li>높낮이 조절 알루미늄 핸들스템</li>
+        <li>벨, 페달 포함</li>
+        </ul>
+        <h4>중량</h4>
+        <ul>
+        <li>약 12kg 이상</li>
+        </ul>
+        <h4>색상</h4>
+        <ul>
+        <li>블랙/레드, 밀크</li>
+        </ul>
+        <h4>기타</h4>
+        <ul>
+        <li>옵션상 98%조립 미선택시 <b>공장출고 반조립박스로 발송</b></li>
+        </ul><hr>
+
+        <!-- [3] 씨티크루저 H7007 -->
+        <h2>선택03</h2>
+        <h3>씨티크루저 H7007</h3>
+        <p>
+        슬릭 하이텐강 하이브리드 자전거 (440, 510)<br>
+        700C / 그립시프트 7단 변속시스템<br>
+        통학용, 출퇴근용, 레저용 권장 / 벨, 페달 포함
+        </p><hr>
+        <a href="https://smartstore.naver.com/seeksports/products/3296139538#revw"
+        target="_blank">
+        <img src="./img/3H7007BR.PNG" alt="[3] 씨티크루저 H7007 블랙/레드">
+        <img src="./img/3H7007WGP.PNG" alt="[3] 씨티크루저 H7007 화이트/그린/퍼플">
+        <img src="./img/3H7007MK.PNG" alt="[3] 씨티크루저 H7007 밀크">
+        </a>
+
+        <h2>SPECIFICATION</h2>
+        <h3 class=specification>상세정보</h3><hr>
+        <h4>스펙</h4>
+        <ul>
+        <li>슬릭 하이텐강 하이브리드 프레임 (440, 510)</li>
+        <li>440차체 : 신장 약 150cm 중반~170cm 후반</li>
+        <li>510차체 : 신장 약 160cm 초반~180cm 초반 승차권장</li>
+        <li>시마노(Shimano) 스프라켓 / 그립시프트 7단 변속시스템</li>
+        <li>제동력 우수한 V-브레이크 / 알루미늄 브레이크 레버</li>
+        <li>700*28 PONELY 컬러 타이어 (프레스타밸브) / 알루미늄 컬러 이중 림</li>
+        <li>체인이탈방지 더블루프 체인가드 장착</li>
+        <li>알루미늄 높낮이조절 핸들스템, 하이브리드 컬러 홀 안장</li>
+        <li>높낮이, 전후 위치조절 가능한 안장 / 물통케이지 장착가능</li>
+        </ul>
+        <h4>중량</h4>
+        <ul>
+        <li>약 13kg 이상</li>
+        </ul>
+        <h4>색상</h4>
+        <ul>
+        <li>블랙/레드, 화이트/그린/퍼플, 밀크</li>
+        </ul>
+        <h4>기타</h4>
+        <ul>
+        <li>옵션상 98%조립 미선택시 <b>공장출고 반조립박스로 발송</b></li>
+        </ul><hr>
+    </div>
+</template>
+<script>
+import data from '@/data'
+export default {
+    name: 'Dummy',
+    data() {
+        const index = this.$route.params.contentId;
+        return {
+            data: data,
+            writer: "",
+            title: "",
+            content: "",
+            index: index,
+            writer: index !== undefined ? data[index].writer : "",
+            title: index !== undefined ? data[index].title : "",
+            content: index !== undefined ? data[index].content : ""
+        }
+    },
+    methods: {
+        write() {
+            this.data.push({
+                writer: this.writer,
+                title: this.title,
+                content: this.content
+            }),
+            this.$router.push({
+                path: '/'
+            })
+        },
+        update() {
+            data[this.index].writer = this.writer,
+            data[this.index].title = this.title,
+            data[this.index].content = this.content,
+            this.$router.push({
+                path: '/'
+            })
+        },
+        write1() {
+            this.data.push({
+                writer: this.writer,
+                title: this.title,
+                content: this.content
+            }),
+            this.$router.push({
+                path: '/'
+            })
+        },
+        update1() {
+            data[this.index].writer = this.writer,
+            data[this.index].title = this.title,
+            data[this.index].content = this.content,
+            this.$router.push({
+                path: '/'
+            })
+        },
+        write2() {
+            this.data.push({
+                writer: this.writer,
+                title: this.title,
+                content: this.content
+            }),
+            this.$router.push({
+                path: '/'
+            })
+        },
+        update2() {
+            data[this.index].writer = this.writer,
+            data[this.index].title = this.title,
+            data[this.index].content = this.content,
+            this.$router.push({
+                path: '/'
+            })
+        },
+        
+        write3() {
+            this.data.push({
+                writer: this.writer,
+                title: this.title,
+                content: this.content
+            }),
+            this.$router.push({
+                path: '/'
+            })
+        },
+        update3() {
+            data[this.index].writer = this.writer,
+            data[this.index].title = this.title,
+            data[this.index].content = this.content,
+            this.$router.push({
+                path: '/'
+            })
+        },
+        write4() {
+            this.data.push({
+                writer: this.writer,
+                title: this.title,
+                content: this.content
+            }),
+            this.$router.push({
+                path: '/'
+            })
+        },
+        update4() {
+            data[this.index].writer = this.writer,
+            data[this.index].title = this.title,
+            data[this.index].content = this.content,
+            this.$router.push({
+                path: '/'
+            })
+        },
+        write5() {
+            this.data.push({
+                writer: this.writer,
+                title: this.title,
+                content: this.content
+            }),
+            this.$router.push({
+                path: '/'
+            })
+        },
+        update5() {
+            data[this.index].writer = this.writer,
+            data[this.index].title = this.title,
+            data[this.index].content = this.content,
+            this.$router.push({
+                path: '/'
+            })
+        }
+    }
+}
+</script>
